6feaecd73f569d0b06871fb4f235248f9bb31454
ShoweR <226817195+55kai12@users.noreply.github.com>
2026-09-15 15:57:57 +0800

    Revert "v2.0.60: 任务页日历区「左右划切周」不再那么容易被误触发"
    This reverts commit 44429b7492777700edb29d51ef3872acdde0e81d.

--- PomodoroFocus/app/build.gradle.kts
+++ PomodoroFocus/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.xian.focus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 260
-        versionName = "2.0.60"
+        versionCode = 259
+        versionName = "2.0.59"
     }
 
     signingConfigs {
